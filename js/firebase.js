a3d547fc02cc0db38308e0f6232bc2752dfaf1f8
Rushgod27 <manoj270399@gmail.com>
2026-02-16 07:15:42 +0530

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -1,19 +1,22 @@
-import { initializeApp }
+import { initializeApp } 
 from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
 
-import { getAuth }
+import { getAuth } 
 from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
 
-import { getFirestore }
+import { getFirestore } 
 from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCX4nJv28LRTtCcH3sjj-LUiXzYfxK3SDM",
   authDomain: "loan-management-system-a5bd8.firebaseapp.com",
-  projectId: "loan-management-system-a5bd8"
+  projectId: "loan-management-system-a5bd8",
+  storageBucket: "loan-management-system-a5bd8.appspot.com",
+  messagingSenderId: "29727312343",
+  appId: "1:29727312343:web:14e58637f47a0994fde392"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app);
+export const db   = getFirestore(app);
