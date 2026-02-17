c9cae5f1a8614f96343e808b1b3135a85668f4d8
Rushgod27 <manoj270399@gmail.com>
2026-02-17 20:11:25 +0530

    Update sw.js

--- js/sw.js
+++ js/sw.js
@@ -1,18 +1,30 @@
-const CACHE_NAME = "agent-app-v1";
+const CACHE_NAME = "Loan Management App";
+
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/agent-dashboard.html",
-  "/borrowers.html",
-  "/borrower-profile.html",
-  "/css/styles.css",
-  "/js/firebase.js"
+  "./",
+  "./index.html",
+  "./agent-dashboard.html",
+  "./borrowers.html",
+  "./borrower-profile.html",
+  "./css/styles.css",
+  "./js/firebase.js"
 ];
 
 self.addEventListener("install", event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME)
+          .map(name => caches.delete(name))
+      );
+    })
   );
 });
 
